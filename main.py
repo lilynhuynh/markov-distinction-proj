507055afe52bd4830baa41d366da440673dc1b09
lilynhuynh <lilynhuynhtx@gmail.com>
2024-09-12 23:45:55 -0400

    Developed initial markov chain set up, added imageio dependency

--- main.py
+++ main.py
@@ -8,8 +8,7 @@
 
 # Import dependencies
 import numpy as np
-
-
+import imageio as img
 
 class BossFight: # Create class for fight sequence
     transition_matrices = {
@@ -30,6 +29,20 @@ class BossFight: # Create class for fight sequence
         }
     }
 
+    def generate_full_fight(self):
+        frames = np.vstack([
+            img.imread("assets/start.gif"),
+            img.imread("assets/phase1_title.gif"),
+            img.imread("assets/phase1_attack.gif"),
+            img.imread("assets/phase2_title.gif"),
+            img.imread("assets/phase2_attack.gif"),
+            img.imread("assets/phase3_title.gif"),
+            img.imread("assets/phase3_attack.gif"),
+            img.imread("assets/knockout.gif")
+        ])
+
+        img.imwrite("examples/weiner_dog_markov_fight.gif", frames)
+
 class WeinerDog: # Create class for WeinerDog based on the phase
     def __init__(self, transition_matrix):
         self.transition_matrix = transition_matrix
@@ -39,9 +52,35 @@ class WeinerDog: # Create class for WeinerDog based on the phase
         """
             Decides which attack to choose based on current attack
 
-            Args: current_attack (str) - current attack that is being done
+            Args:
+            - current_attack (str) - current attack that is being done
         """
         return np.random.choice(
             self.attacks,
             p=[self.transition_matrix[current_attack][next_attack] for next_attack in self.attacks]
         )
+    
+    def create_attack_sequence(self, current_attack="slinky_smash", sequence_length=10):
+        """
+            Generate a attack sequence based on the given transition matrix
+            for the phase
+
+            Args:
+            - current_attack (str) - pre-set to "slinky smash"
+            - sequence_length (int) - pre-set to 10
+        """
+        attack_sequence = []
+
+        while len(attack_sequence) < sequence_length:
+            next_attack = self.get_next_attack(current_attack)
+            attack_sequence.append(next_attack)
+            current_attack = next_attack
+
+        return attack_sequence
+    
+    def generate_phase_sequence(self, attack_sequence, phase_num):
+        animated_images = []
+        for attack in attack_sequence:
+            image_path = str("assets/" + attack + ".gif")
+            animated_images.append(img.imread(image_path))
+        img.mimsave(str("assets/phase" + phase_num + "_attack.gif"), animated_images)
